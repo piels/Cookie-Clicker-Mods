420debe5ac05c26e015256422af8e6518c0f82d4
piels <greg@flyingpawn.com>
2020-11-11 08:43:14 -0500

    Create JQB.js

---
+++ JQB.js
@@ -0,0 +1,13 @@
+/** Is this how JavaScript comments work?
+
+Quad states
+Empty ──────────────────────┬──┐
+└── Growing QBs             │  │
+    └── Breeding QBs ─────┐ │  │
+        ├── Wrong Sprout ─┘ │  │
+        ├── Dying QBs ──────┘  │
+        └── JQB Sprout         │
+            └── Mature JQB ────┘
+
+
+**/

0b7834dc1440e3f3ca5890f77212c2e63634b990
Gregory King <greg@flyingpawn.com>
2024-05-05 11:38:40 -0400

    Update mod.js

--- SimpleGardener/mod.js
+++ SimpleGardener/mod.js
@@ -18,6 +18,7 @@ const simpleGarden = {
 const goldCloverGrower = {
   g: simpleGarden.g,
   work: function () {
+    console.log("work goldCloverGrower")
     let myGoldClover = this.g.plantsById[5];
     let myNurseTulip = this.g.plantsById[16];
     if (Game.cookies >= this.g.getCost(myGoldClover) * 1000) {  
@@ -29,101 +30,12 @@ const goldCloverGrower = {
           }
         }
       }
+    } else {
+      console.log("not enough cookies to plant")
     }
   }
 }
 
-
-const seedCollector = {
-  g: simpleGarden.g,
-  work: function () {
-    // check each tile for new seeds
-    console.log("seed collector");
-    for (let x = 0; x < 6; x++) {
-      for (let y = 0; y < 6; y++) {
-        var tile = this.g.getTile(x, y);
-        if (tile[0] > 1) {
-          var plant = this.g.plantsById[tile[0] - 1];
-          var age = tile[1];
-          //console.log(x,y,plant.name,age);
-          if (!plant.unlocked && (age > plant.mature)) {
-            this.g.harvest(x, y);
-            console.log("harvested " + plant.name);
-          }
-        }
-      }
-    }
-  }
-}
-
-const lastChancer = {
-  g: simpleGarden.g,
-  work: function () {
-    // check each tile for new seeds
-    console.log("last chancer");
-    for (let x = 0; x < 6; x++) {
-      for (let y = 0; y < 6; y++) {
-        var tile = this.g.getTile(x, y);
-        if (tile[0] > 1) {
-          var plant = this.g.plantsById[tile[0] - 1];
-          var age = tile[1];
-          //console.log(x,y,plant.name,age);
-          if ((age + plant.ageTick + plant.ageTickR > 100) && !(plant.onDie)) {
-            this.g.harvest(x, y);
-            console.log("harvested " + plant.name);
-          }
-        }
-      }
-    }
-  }
-}
-
-
-const weedEater = {
-  g: simpleGarden.g,
-  unlockedPlantsLastCheck: 0,
-  depletedPlants: new WeakMap(),
-  work: function () {
-    console.log("weed eater");
-    if (this.unlockedPlantsLastCheck < this.g.plantsUnlockedN) {
-      console.log("checking unlocked plants");
-      for (var p of this.g.plantsById) {
-        if (p.unlocked) {
-          // console.log(p.name, "children:");
-          let lockedChildren = 0;
-          for (var cname of p.children) {
-            // console.log(cname);
-            if (!this.g.plants[cname].unlocked) { lockedChildren++; }
-          }
-          console.log(p.name, "children:", p.children.length, "locked:", lockedChildren);
-          if (lockedChildren == 0) this.depletedPlants.set(p, true);
-        }
-      }
-      this.unlockedPlantsLastCheck = this.g.plantsUnlockedN;
-      console.log(this.depletedPlants);
-    }
-    for (let x = 0; x < 6; x++) {
-      for (let y = 0; y < 6; y++) {
-        var tile = this.g.getTile(x, y);
-        if (tile[0] > 0) {
-          //what kind of plant is it?
-          //what offspring can it have?
-          //do I have seeds for all of them?
-          //
-          var plant = this.g.plantsById[tile[0] - 1];
-          if (this.depletedPlants.get(plant)) {
-            this.g.harvest(x, y);
-            console.log("weeded " + plant.name);
-          }
-        }
-      }
-    }
-  }
-
-
-}
-
-
-// simpleGarden.workers.push(seedCollector);
+simpleGarden.workers.push(goldCloverGrower);
 
 simpleGarden.tickLoop();
